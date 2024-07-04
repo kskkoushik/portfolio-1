641d23e30f364e6f62c886d36bd747c10243d3fc
Vmihira <129678463+Vmihira@users.noreply.github.com>
2024-07-04 21:03:25 +0530

    Update AboutSection.jsx

--- app/components/AboutSection.jsx
+++ app/components/AboutSection.jsx
@@ -10,35 +10,35 @@ const TAB_DATA = [
     content: (
       <ul className="list-disc font-semibold pl-2 text-[#4F6F52]">
         <li>HTML<br />
-        <progress className="progress progress-info w-56" value="97" max="100"></progress>
+        <progress className="progress progress-success w-56" value="97" max="100"></progress>
         95
         </li>
         <li>CSS<br />
-        <progress className="progress progress-info w-56" value="80" max="100"></progress>
+        <progress className="progress progress-success w-56" value="80" max="100"></progress>
         80
         </li>
         <li>JavaScript<br />
-        <progress className="progress progress-info w-56" value="80" max="100"></progress>
+        <progress className="progress progress-success w-56" value="80" max="100"></progress>
         80
         </li>
         <li>Bootstrap<br />
-        <progress className="progress progress-info w-56" value="75" max="100"></progress>
+        <progress className="progress progress-success w-56" value="75" max="100"></progress>
         75
         </li>
         <li>TailwindCSS<br />
-        <progress className="progress progress-info w-56" value="85" max="100"></progress>
+        <progress className="progress progress-success w-56" value="85" max="100"></progress>
         85
         </li>
         <li>React.js<br />
-        <progress className="progress progress-info w-56" value="75" max="100"></progress>
+        <progress className="progress progress-success w-56" value="75" max="100"></progress>
         75
         </li>
         <li>Next.js<br />
-        <progress className="progress progress-info w-56" value="85" max="100"></progress>
+        <progress className="progress progress-success w-56" value="85" max="100"></progress>
         85
         </li>
         <li>MongoDB<br />
-        <progress className="progress progress-info w-56" value="80" max="100"></progress>
+        <progress className="progress progress-success w-56" value="80" max="100"></progress>
         80
         </li>
       </ul>
@@ -59,7 +59,6 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc font-semibold pl-2 text-[#4F6F52]">
-        <li>Web development from IBM SkillsBuild</li>
         <li>MongoDB in Python</li>
         <li>Python from Infosys Springboard</li>
         <li>Basics of Generative AI From LinkedIn Learning</li>
@@ -82,11 +81,10 @@ const AboutSection = () => {
   return (
     <section>
         <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-            <Image src="/images/about_image.png" width={500} height={500} />
+            <Image src="/images/about_image_3.png" width={500} height={500} />
             <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
-                {/*<h2 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#199df5] via-blue-300 to-[#199df5] mb-4">About Me</h2>*/}
                 <h2 className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-[#4F6F52] via-green-200 to-[#4F6F52] mb-4">About Me</h2>
-                <p className="text-lg lg:text-lg text-[#4F6F52]">
+                <p className="text-base lg:text-lg text-[#4F6F52]">
                     I am a passionate web developer, intrested in creating responsive web applications. I work with HTML, CSS, JavaScript, TailwindCSS, React.js, Next.js, MongoDB. Being a web developer, I am always intrested in adding new skills in my skill set.
                 </p>
                 <div className="flex flex-row justify-start mt-5">
