f127a3b9e2030d61a7f75458c5c72fa23eadc050
Vmihira <129678463+Vmihira@users.noreply.github.com>
2024-07-04 20:45:32 +0530

    Update EmailSection.jsx

--- app/components/EmailSection.jsx
+++ app/components/EmailSection.jsx
@@ -9,10 +9,10 @@ const EmailSection = () => {
       {/*<div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>*/}
         <div>
             {/*<h2 className="text-lg font-bold text-[#5297cb] my-2 text-center">Contact Me!!</h2>*/}
-            <h2 className="text-4xl font-bold text-[#5297cb] mt-4 mb-5 lg:pl-28">
+            <h2 className="text-4xl font-bold text-[#4F6F52] mt-4 mb-5 lg:pl-28">
               Reach Me!!
             </h2>
-            <p className="text-[#294457] text-lg md-4 max-w-md lg:pl-28">
+            <p className="text-[#266631] text-lg md-4 max-w-md lg:pl-28">
                 {" "}
                 I am always interested in collaborating on new projects and exploring exciting opportunities. Feel free to reach out to me via email or connect with me on LinkedIn...
             </p>
@@ -31,16 +31,16 @@ const EmailSection = () => {
         <div>
             <form className="flex flex-col gap-6 lg:pr-36">
               <div className="mb-4">
-                <label htmlFor="email" type="email" className="text-[#5297cb] block mb-2 text-sm font-medium">Your email</label>
+                <label htmlFor="email" type="email" className="text-[#266631] block mb-2 text-sm font-medium">Your email</label>
                 <input type="email" id="email" required placeholder="abc@gmail.com" className="bg-[#b2dcd4] border border-[#3335F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" />
               </div>
 
               <div className="mb-6">
-                <label htmlFor="message" className="text-[#5297cb] block mb-2 text-sm font-medium">Subject</label>
+                <label htmlFor="message" className="text-[#266631] block mb-2 text-sm font-medium">Subject</label>
                 <textarea name="message" id="message" required placeholder="Text me!!" className="bg-[#b2dcd4] border border-[#3335F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5" />
               </div> 
 
-              <button type="button" className="border-black border-2 bg-gradient-to-br from-[#199df5] via-blue-100 to-[#199df5] text-[#010101] font-bold py-2.5 px-5 rounded-lg w-full">
+              <button type="button" className="border-black border-2 bg-gradient-to-br from-[#4F6F52] via-green-200 to-[#4F6F52] text-[#010101] font-bold py-2.5 px-5 rounded-lg w-full">
                 <a href={`mailto:vinjamurimihira@gmail.com`}>Message Me!!</a>
               </button>
 
